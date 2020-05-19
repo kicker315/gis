6546c633ff6e4b40cf78a023d36a53d52735b182
Skye <ningsk0115@hotmail.com>
2020-05-19 13:25:09 +0800

    项目初始化

---
+++ app/src/main/java/com/zydcc/zrdc/base/App.kt
@@ -0,0 +1,18 @@
+package com.zydcc.zrdc.base
+
+import android.app.Application
+import androidx.multidex.MultiDexApplication
+
+/**
+ * =======================================
+ * App
+ * Create by ningsikai 2020/5/19:1:17 PM
+ * ========================================
+ */
+class App : MultiDexApplication() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+}
