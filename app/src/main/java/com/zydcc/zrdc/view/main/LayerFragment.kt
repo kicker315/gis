4c0c84a80e492fd27d1b1ccb066e3c3976241ae7
Skye <ningsk0115@hotmail.com>
2020-06-16 14:34:08 +0800

    默认底图唯一
    目前是一个影像底图+ 多个操作图层

--- app/src/main/java/com/zydcc/zrdc/view/main/LayerFragment.kt
+++ app/src/main/java/com/zydcc/zrdc/view/main/LayerFragment.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.RecyclerView
@@ -78,6 +79,10 @@ class LayerFragment : Fragment() {
                 }
 
                 override fun addTpk() {
+                    if (mTpkManagerAdapter.itemCount == 1) {
+                        Toast.makeText(requireContext(), "请先移除已添加的tpk再添加",Toast.LENGTH_SHORT).show()
+                        return
+                    }
                     if (mTpkChooseDialog == null) {
                         mTpkChooseDialog = DatasourceChooseDialog()
                         val bundle = Bundle()
