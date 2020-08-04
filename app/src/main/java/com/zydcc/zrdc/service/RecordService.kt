0cd7c790b7f7adf8b73cc940717b699bf1f382e8
Skye <ningsk0115@hotmail.com>
2020-08-04 15:57:29 +0800

    添加录音服务

---
+++ app/src/main/java/com/zydcc/zrdc/service/RecordService.kt
@@ -0,0 +1,10 @@
+package com.zydcc.zrdc.service
+
+/**
+ * =======================================
+ *
+ * Create by ningsikai 2020/8/4:2:47 PM
+ * ========================================
+ */
+class RecordService {
+}
